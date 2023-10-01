c1c66401a14f0d0541122c1de59106c3411a5ec8
liamdawson <liamrdawson@gmail.com>
2023-10-01 19:42:58 +0100

    Add error handling for form.

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,3 +1,4 @@
+import type { Post } from '@prisma/client'
 import { prisma } from '~/db.server'
 
 export async function getPosts() {
@@ -6,4 +7,10 @@ export async function getPosts() {
 
 export async function getPost(slug: string) {
 	return prisma.post.findUnique({ where: { slug } })
+}
+
+export async function createPost(
+	post: Pick<Post, 'slug' | 'title' | 'markdown'>
+) {
+	return prisma.post.create({ data: post })
 }
--- app/routes/posts.admin.new.tsx
+++ app/routes/posts.admin.new.tsx
@@ -1,3 +1,64 @@
+import type { ActionFunctionArgs } from '@remix-run/node'
+import { redirect, json } from '@remix-run/node'
+import { Form, useActionData } from '@remix-run/react'
+import invariant from 'tiny-invariant'
+
+import { createPost } from '~/models/post.server'
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const formData = await request.formData()
+
+	const title = formData.get('title')
+	const slug = formData.get('slug')
+	const markdown = formData.get('markdown')
+
+	const errors = {
+		title: title ? null : 'Title is required',
+		slug: slug ? null : 'Slug is required',
+		markdown: markdown ? null : 'Markdown is required',
+	}
+	const hasErrors = Object.values(errors).some((errorMessage) => errorMessage)
+	if (hasErrors) {
+		return json(errors)
+	}
+
+	invariant(typeof title === 'string', 'title must be a string')
+	invariant(typeof slug === 'string', 'slug must be a string')
+	invariant(typeof markdown === 'string', 'markdown must be a string')
+
+	await createPost({ title, slug, markdown })
+
+	return redirect('/posts/admin')
+}
+
 export default function NewPost() {
-	return <h2>New Post</h2>
+	const errors = useActionData<typeof action>()
+	console.log(errors)
+	return (
+		<Form method='post'>
+			<p>
+				<label>
+					Post Title:
+					{errors?.title ? <em>{errors.title}</em> : null}
+					<input type='text' name='title' />
+				</label>
+			</p>
+			<p>
+				<label>
+					Post Slug:
+					{errors?.slug ? <em>{errors.slug}</em> : null}
+					<input type='text' name='slug' />
+				</label>
+			</p>
+			<p>
+				<label htmlFor='markdown'>Markdown: </label>
+				<br />
+				{errors?.markdown ? <em>{errors.markdown}</em> : null}
+				<textarea id='markdown' rows={20} name='markdown' />
+			</p>
+			<p className='text-right'>
+				<button type='submit'>Create Post</button>
+			</p>
+		</Form>
+	)
 }
