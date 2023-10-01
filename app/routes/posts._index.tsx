64a836c018de65182427b48f9e4a0739e9915834
liamdawson <liamrdawson@gmail.com>
2023-10-01 18:56:56 +0100

    Set up /admin route.

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,27 +1,32 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/models/post.server";
+import { json } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { getPosts } from '~/models/post.server'
 
 export const loader = async () => {
-  // Loader functions are the backend "API" for their component, and it's already wired up for you through useLoaderData.
-  // You could have something else here that gets your posts from some other location.
-  return json({
-    posts: await getPosts(),
-  });
-};
+	// Loader functions are the backend "API" for their component, and it's already wired up for you through useLoaderData.
+	// You could have something else here that gets your posts from some other location.
+	return json({
+		posts: await getPosts(),
+	})
+}
 
 export default function Posts() {
-  const { posts } = useLoaderData<typeof loader>();
-  return (
-    <main>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link to={post.slug}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+	const { posts } = useLoaderData<typeof loader>()
+	return (
+		<main>
+			<h1>Posts</h1>
+			<ul>
+				{posts.map((post) => (
+					<li key={post.slug}>
+						<Link to={post.slug}>{post.title}</Link>
+					</li>
+				))}
+			</ul>
+			<div>
+				<Link to='admin' className='text-red-600 underline'>
+					Admin
+				</Link>
+			</div>
+		</main>
+	)
 }
