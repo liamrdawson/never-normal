308ce51a48510c8d7166a1a793006889b49ea459
liamdawson <liamrdawson@gmail.com>
2023-11-18 13:00:41 +0000

    test: :white_check_mark: createSlugFromTitle
    Add unit tests for createSlugFromTitle

--- app/utils/tests/utils.test.ts
+++ app/utils/tests/utils.test.ts
@@ -5,4 +5,47 @@ describe('createSlugFromTitle', () => {
 		const slug = createSlugFromTitle(title)
 		expect(slug).toBe('this-is-a-post-title')
 	})
+
+	it('should handle a title with special characters', () => {
+		const title = 'Special $@!Characters'
+		const slug = 'special-$@!characters'
+
+		const result = createSlugFromTitle(title)
+
+		expect(result).toEqual(slug)
+	})
+
+	it('should throw an error for an empty title', () => {
+		const emptyTitle = ''
+		const expectedErrorMessage =
+			/^Invalid input: Title cannot be a string of only spaces.$/
+
+		expect(() => createSlugFromTitle(emptyTitle)).toThrow(expectedErrorMessage)
+	})
+
+	it('should throw an error for a title with only spaces', () => {
+		const titleWithSpaces = '      '
+		const expectedErrorMessage =
+			/^Invalid input: Title cannot be a string of only spaces.$/
+
+		expect(() => createSlugFromTitle(titleWithSpaces)).toThrow(
+			expectedErrorMessage
+		)
+	})
+
+	it('should throw an error if the title is too short', () => {
+		const shortTitle = 'No'
+		const expectedErrorMessage =
+			/^Invalid input: "No" is too short. Title needs to be longer than 4 characters.$/
+		expect(() => createSlugFromTitle(shortTitle)).toThrow(expectedErrorMessage)
+	})
+
+	it('should throw an error if the title contains only special characters', () => {
+		const specialCharacters = '!@£$%'
+		const expectedErrorMessage =
+			/^Invalid input: "!@£\$%" contains only special characters.$/
+		expect(() => createSlugFromTitle(specialCharacters)).toThrow(
+			expectedErrorMessage
+		)
+	})
 })
