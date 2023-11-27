b15a0527a1f98905c017edf797c7fcd677bcb804
liamdawson <liamrdawson@gmail.com>
2023-11-27 18:26:26 +0000

    feat: :passport_control: Get Bearer token from Google API.

---
+++ app/utils/auth.ts
@@ -0,0 +1,54 @@
+/**
+ *  1. Create a JWT which includes:
+ *      a. a header
+ *      b. a claim set
+ *      c. a signature
+ *  2. Request an access token from the Google OAuth 2.0 Authorization Server.
+ *  3. Handle the JSON response that the Authorization Server returns.
+ */
+
+import jwt from 'jsonwebtoken'
+
+export function getSignedJSONWebToken() {
+	const secret = process.env.GOOGLE_API_SECRET
+	const keyId = process.env.GOOGLE_API_PRIVATE_KEY_ID
+
+	const claimSet = {
+		iss: 'calendar@never-normal.iam.gserviceaccount.com',
+		sub: 'liam.dawson@nevernormalcommerce.com',
+		scope: 'https://www.googleapis.com/auth/calendar',
+		aud: 'https://oauth2.googleapis.com/token',
+		exp: Math.floor(Date.now() / 1000) + 3600,
+		iat: Math.floor(Date.now() / 1000),
+	}
+
+	const token = jwt.sign(claimSet, secret as string, {
+		header: {
+			alg: 'RS256',
+			kid: keyId,
+			typ: 'JWT',
+		},
+	})
+
+	return token
+}
+
+export async function getBearerToken() {
+	const signedJWT = getSignedJSONWebToken()
+
+	const data = {
+		grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+		assertion: signedJWT,
+	}
+	const response = await fetch('https://oauth2.googleapis.com/token', {
+		method: 'POST',
+		body: new URLSearchParams(data),
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+	})
+		.then((res) => res.json())
+		.catch((err) => console.error(err))
+
+	return response
+}
